ddc7d3e6ec267599edddc6a0b0b0f100b3cfa493
maliabarker <89502882+maliabarker@users.noreply.github.com>
2021-09-22 17:14:40 -0700

    added buttons onto people input

--- scripts.js
+++ scripts.js
@@ -4,17 +4,23 @@ console.log(billInput);
 const tipInput = document.querySelector('#input-tip');
 console.log(tipInput);
 const peopleInput = document.querySelector('#input-people');
+peopleInput.defaultValue = 0;
 console.log(peopleInput);
 const displayTip = document.querySelector('#display-tip');
 console.log(displayTip);
 const displayTotal = document.querySelector('#display-total');
 console.log(displayTotal);
 const displayPerPerson = document.querySelector('#display-per-person');
+// DOM elements for the buttons
+const addPeople = document.querySelector('#plus-sign');
+const subtractPeople = document.querySelector('#minus-sign');
 
 // add event listeners for each input element
 billInput.addEventListener('input', handleBill);
 tipInput.addEventListener('input', handleBill);
 peopleInput.addEventListener('input', handleBill);
+addPeople.addEventListener("click", addPerson);
+subtractPeople.addEventListener("click", subtractPerson);
 
 // function to get value of each input and calculate tip, total, and cost per person
 function handleBill(e) {
@@ -35,4 +41,21 @@ function handleBill(e) {
     displayTotal.innerHTML = `Total: $${total.toFixed(2)}`;
     displayPerPerson.innerHTML = `Cost per Person: $${perPerson.toFixed(2)}`;
 };
+
+function addPerson(e) {
+    let party = parseInt(peopleInput.value);
+    party += 1;
+    console.log('added person');
+    peopleInput.value = party;
+    handleBill();
+}
+
+function subtractPerson(e) {
+    let party = parseInt(peopleInput.value)
+    party -= 1;
+    console.log('subtracted person');
+    peopleInput.value = party;
+    handleBill();
+}
+
 
